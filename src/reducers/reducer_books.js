52efd35acb134b35f2c8f24584c829b378425256
Marek Strejczek <marek.strejczek@tomtom.com>
2018-09-17 22:51:45 +0200

    Implemented book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Harry Potter'},
+        { title: 'Heart of Darkness' },
+        { title: 'Dark Matter' },
+        { title: 'Something to Declare' }
+    ]
+}
