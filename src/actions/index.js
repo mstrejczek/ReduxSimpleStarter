ea2860d6dbd5e1c117988fe2966a720c681702ec
Marek Strejczek <marek.strejczek@tomtom.com>
2018-10-09 00:22:54 +0200

    Early support for graphs - sparklines

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_KEY = 'FILL ME IN';
+const API_KEY = 'aef4a902ef0761bf31b6207d1a5c266e';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
